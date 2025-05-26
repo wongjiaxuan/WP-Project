138a2681d7bc5b838ae696d14aa7bc544c64123d
Wong Jia Xuan <148445032+wongjiaxuan@users.noreply.github.com>
2025-05-26 19:33:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuIcon = document.getElementById('menuicon');
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+    
+menuIcon.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    menuIcon.classList.toggle('rotate');
+});
+
+document.addEventListener('click', (event) => {
+    if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+        // Hide the menu if the click is outside of the menu and icon
+        menu.classList.remove('show');
+        menuIcon.classList.remove('rotate');
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+    document.querySelectorAll('#current-year').forEach(el => {
+        el.textContent = currentYear;
+    });
+});
