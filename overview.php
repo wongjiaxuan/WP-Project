d06a4645a91b401e38750939ffb4c3585e263965
YONG JING WEN <152403691+JWEN0518@users.noreply.github.com>
2025-06-30 18:34:03 +0800

    Update overview.php

--- overview.php
+++ overview.php
@@ -71,7 +71,7 @@
             <ul class="menu">
                 <li><a href="home.php">Home</a></li>
                 <li><a href="input.php">Transaction Input</a></li>
-                <li><a href="set_budget">Monthly Budget</a></li>
+                <li><a href="set_budget.html">Monthly Budget</a></li>
                 <li><a href="overview.php" class="active">Transaction Overview</a></li>
                 <li><a href="dashboard.html">Finance Dashboard</a></li>
             </ul>
