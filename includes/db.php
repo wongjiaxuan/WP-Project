2a9a74c897ce9c3ec355a959be7f8fcd619e5cb8
Lee Lai Queen <leelqueeno@gmail.com>
2025-07-03 04:18:40 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -4,10 +4,8 @@ $user = 'root';
 $pass = ''; // XAMPP default is no password
 $dbname = 'jimatmaster';
 
-// Create connection
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
